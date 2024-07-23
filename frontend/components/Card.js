c1a2c91685a98f79e84ec2978ecb6555dda63725
jesusmedero <jjmowd@gamil.com>
2024-07-22 20:58:42 -0700

    Sprint 6 mod 4

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import Figure from './Figure'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+ border: 1px solid black;
+ border-radius: 12px;
+ padding: 2rem;
+
+ h2 {
+  color: ${pr => pr.$color};
+  font-size: 2.5 em;
+  margin: 0 0 1rem 0;
+ }
+
+ p {
+  margin:  0 0 1.5rem 0;
+  &::first-line {
+    font-size:  1.5em;
+  }
+ }
+`
 
 export default function Card({title, text, imageURL, date}) {
     return(
-        <div className='card'>
+        <StyledCard $color="blue" className='Card'>
           <h2>{title}</h2>
           <p>{text}</p>
           <Figure
           imageURL={imageURL}
           caption={date}
           />       
-        </div>
+        </StyledCard>
     )
 }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledFigure = styled.figure`
+  max-width: 60%;
+
+  figcaption {
+    font-style: italic;
+ }
+`
 
 export default function Figure({imageURL, caption }) {
     return (
-        <figure>
-            <img scr={imageURL} />
+        <StyledFigure>
+            <img src={imageURL} />
             <figcaption>Awesome pic taken on {caption}</figcaption>
-        </figure>
+        </StyledFigure>
     )
 }
